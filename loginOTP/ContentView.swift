a3d4c58a6bd880c2f32fcd14e4e4b2331f84eaaf
Jennilyn <jennilyn@fitprotracker.com>
2021-03-29 00:25:51 -0600

    Initial Commit

---
+++ loginOTP/ContentView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("status") var logged = false
+    
+    var body: some View {
+        NavigationView{
+            if logged{
+                VStack{
+                    Text("User Logged In").navigationTitle("Home").navigationBarHidden(false).preferredColorScheme(.light)
+                    Button(action:{
+                        logged = false
+                    }, label:{
+                        Text("Logout")
+                    })
+                }
+            }
+            else{
+                //Will add code later
+               // Text("Login Screen")
+                LoginView()
+                    .preferredColorScheme(.light)
+                    .navigationBarHidden(true)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ loginOTP/loginOTPApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct loginOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
